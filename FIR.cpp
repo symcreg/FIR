ebfaff33f1efab0ce8caacd0f19a558cdcdd9eb6
symcreg <symcreg@outlook.com>
2022-10-25 09:40:14 +0800

    上csdn了解了权值法 改写了权值的计算（未完）

--- FIR.cpp
+++ FIR.cpp
@@ -12,6 +12,10 @@ struct Index
     int x;
     int y;
 };
+void PrintBoard(int board[height][width]);
+int IsWin(int board[height][width]);
+void GetImp(int y, int x, int imp[height][width], int board[height][width]);
+void Computer(int board[height][width], int imp[height][width]);
 void PrintBoard(int board[height][width]) {//打印棋盘
     printf("18*9\n");
     for (int i = 0; i < height; i++) {
@@ -68,66 +72,90 @@ int IsWin(int board[height][width]) {
     }
     return 0;
 }
-void GoImp(int y,int x,int imp[height][width],int board[height][width]) {
-    //权重计算并赋值,将八个方向的数值相加
-    int sum_x = 0;
-    int sum_y = 0;
-    int sum_z = 0;
-    for (int z = -5; z < 5; z++) {
-        
-        if (x + z >= 0 && x + z < width ) {
-            sum_x += abs(board[y][x + z]);//横向
+void GetImp(int y,int x,int imp[height][width],int board[height][width]) {
+    //权重计算,八方相加
+    int color = -3;
+    int sum_left = 0;
+    int sum_right = 0;
+    int sum_up = 0;
+    int sum_down = 0;
+    //计算左方的权值
+    for (int i = 1;x-i>=0; i++) {
+        if (board[y][x - i] == 0) {//相邻为空
+            break;
         }
-        
-        if (y + z >= 0 && y + z < height) {
-            sum_y += abs(board[y + z][x]);//纵向
+        else if (board[y][x-i]==color||color==-3) {//相邻相同颜色的棋子
+            color = board[y][x - i];
+            sum_left++;
         }
-        if (x + z >= 0 && y + z >= 0 && x + z < width && y + z < height) {
-            sum_z += abs(board[y + z][x + z]);//斜向
+        else {//遇到颜色不同的棋子
+            break;
         }
     }
+    //计算右方的权值
+    color = -3;
+    for (int i = 1; x + i < width; i++) {
+        if (board[y][x + i] == 0) {//相邻为空
+            break;
+        }
+        else if (board[y][x + i] == color || color == -3) {//相邻相同颜色的棋子
+            color = board[y][x + i];
+            sum_right++;
+        }
+        else {//遇到颜色不同的棋子
+            break;
+        }
+    }
+    //计算上方的权值
+    color = -3;
+    for (int i = 1; y - i >= 0; i++) {
+        if (board[y-i][x] == 0) {//相邻为空
+            break;
+        }
+        else if (board[y-i][x] == color || color == -3) {//相邻相同颜色的棋子
+            color = board[y-i][x];
+            sum_up++;
+        }
+        else {//遇到颜色不同的棋子
+            break;
+        }
+    }
+    //计算下方的权值
 
-    imp[y][x] = abs(sum_x) + abs(sum_y) + abs(sum_z);
-    for (int z = 0;y-z>=0; z++) {
-        if (y-z > 0) {
-            if (board[y-z - 1][x] != 0 || board[y+z + 1][x] != 0 || board[y+z + 1][x+z + 1] != 0) {
-                imp[y][x]++;
-            }
-        }
-        if (x-z > 0) {
-            if (board[y][x - z - 1] != 0 || board[y][x+z + 1] != 0 || board[y+z + 1][x+z + 1] != 0) {
-                imp[y][x]++;
-            }
-        }
-        if (y-z > 0 && x-z > 0) {
-            if (board[y-z - 1][x-z - 1] != 0) {
-                imp[y][x]++;
-            }
-        }
-    }
+    //计算左上方的权值
+
+    //计算左下方的权值
+
+    //计算右上方的权值
+
+    //计算右下方的权值
+
+
+    memset(imp, 0, sizeof(imp));
+    
 }
 void Computer(int board[height][width], int imp[height][width]) {
     Index index = {0,0,0};//用以标记权重最大的位置
-    //根据数值来进行imp赋值，越小或越大imp越大，优先越大出棋
+    //根据imp权值下棋
     
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
-            
-                GoImp( i, j, imp,board);
+            if (board[i][j] == 0) {//该位置为空，计算权值
+                GetImp(i, j, imp, board);
+            }
         }
     }
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
             //遍历权重后选取最高的位置出棋
-            if (imp[i][j] > index.imp && board[i][j]==0) {
+            if (imp[i][j] > index.imp) {
+                index.imp = imp[i][j];
                 index.y = i;
                 index.x = j;
-                goto end;
             }
         }
     }
-    end:
-    board[index.y][index.x] = -1;
+    board[index.y][index.x] = -1;//下棋
 }
 int main()
 {
